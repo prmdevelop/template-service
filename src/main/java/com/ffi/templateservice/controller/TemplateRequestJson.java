a8bbdf50e8ef1c1bc67c07ad98427ced7481f4c9
ritesh <ritesh@prmfincon.com>
2019-04-15 10:31:22 +0530

    removerd dto

--- src/main/java/com/ffi/templateservice/controller/TemplateRequestJson.java
+++ src/main/java/com/ffi/templateservice/controller/TemplateRequestJson.java
@@ -1,9 +1,6 @@
 package com.ffi.templateservice.controller;
 
 import java.io.Serializable;
-import java.util.List;
-
-import com.ffi.templateservice.dto.TemplateFinancialDTO;
 
 public class TemplateRequestJson implements Serializable{
 
@@ -11,30 +8,13 @@ public class TemplateRequestJson implements Serializable{
 
 	private String templateName;
 	
-	private List<TemplateFinancialDTO> data;
-
 	public String getTemplateName() {
 		return templateName;
 	}
 
-	public void setTemplateName(String templateName) {
-		this.templateName = templateName;
-	}
-
-	public List<TemplateFinancialDTO> getData() {
-		return data;
-	}
-
-	public void setData(List<TemplateFinancialDTO> data) {
-		this.data = data;
-	}
-	
 	@Override
 	public String toString() {
-		return "TemplateRequestJson [templateName=" + templateName + ", data=" + data + "]";
+		return "TemplateRequestJson [templateName=" + templateName + "]";
 	}
-
-	
-
 	
 }
